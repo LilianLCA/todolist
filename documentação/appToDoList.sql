6dfd44dc93a60d5bcd7d8e8d84ea09ef581af549
Lilian <liliancarolina.assuncao@gmail.com>
2022-02-23 23:37:43 -0300

    Novas informações adicionadas na API.

---
+++ documentação/appToDoList.sql
@@ -0,0 +1,1 @@
+SELECT * FROM db_apptodolist.tb_usuario;
--- src/main/java/br/com/apptodolist/todolist/TodolistApplication.java
+++ src/main/java/br/com/apptodolist/todolist/TodolistApplication.java
@@ -2,6 +2,8 @@ package br.com.apptodolist.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class TodolistApplication {
@@ -10,4 +12,9 @@ public class TodolistApplication {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
 
+	@GetMapping
+	public ModelAndView swaggerUi () {
+			
+		return new ModelAndView("redirect:/swagger-ui/");
+		}
 }
